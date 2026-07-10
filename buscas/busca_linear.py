12090199b4801a0348e0cc82ec92a71bc77ba00a
Caio Vinícius <caiov.devwork@gmail.com>
2026-07-10 20:34:35 -0300

    feat: implementa algoritmo de busca linear

--- buscas/busca_linear.py
+++ buscas/busca_linear.py
@@ -1,11 +1,12 @@
-def busca_linear(lista, valor_escolhido):
-    for numeros in lista:
-        if valor_escolhido == numeros:
-            return valor_escolhido
-        return -1
-    
-numeros = [2,4,5,6,7,810,34]
+def busca_linear(lista, valor):
+    for indice, numero in enumerate(lista):
+        if valor == numero:
+            return indice
+    return -1
 
 
-result = busca_linear(numeros, 1)
+lista_inteiros = [2, 6, 10, 15, 25, 40]
+result = busca_linear(lista_inteiros, 2)
 print(result)
+
+
