0c36dba72fe9d352135f1fec55c07673368d5e82
Caio Vinícius <caiov.devwork@gmail.com>
2026-07-20 21:35:06 -0300

    feat(linked-list): implement append and find methods

---
+++ estruturas/linked_lists/linked_list.py
@@ -0,0 +1,46 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None    
+
+
+    def prepend(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+
+    def append(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+
+        current = self.head
+
+        while current.next is not None:
+            current = current.next
+
+        current.next = new_node
+        
+
+    def find(self, data):
+        current = self.head
+
+        while current is not None:
+            if current.data == data:
+                return current
+
+            current = current.next
+
+        return None
+
+
+
+
