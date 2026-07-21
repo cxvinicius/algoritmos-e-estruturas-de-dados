a6bcfd11be21bff1999a87b766e065f961849e36
Caio Vinícius <caiov.devwork@gmail.com>
2026-07-21 20:10:04 -0300

    style(queue): improve code formatting

--- estruturas/queue/queue_structure.py
+++ estruturas/queue/queue_structure.py
@@ -2,24 +2,30 @@ class Queue:
     def __init__(self):
         self._queue = []
     
+
     def enqueue(self, valor):
         self._queue.append(valor)
+
 
     def dequeue(self):
         if self.is_empty():
             return None
         return self._queue.pop(0)
     
+
     def is_empty(self):
         return not self._queue
     
+
     def peek(self):
         if self.is_empty():
             return None
         return self._queue[0]
     
+
     def __len__(self):
         return len(self._queue)
+    
     
     def __str__(self):
         if self.is_empty():
